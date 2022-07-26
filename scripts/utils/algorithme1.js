295375ffbc9d3160aa92c2a2ae45f1277a51c140
Jean Marie Hitayezu <hitayezujeanmarie@gmail.com>
2022-07-26 09:37:31 +0200

    Implementation of the second algorithm

--- scripts/utils/algorithme1.js
+++ scripts/utils/algorithme1.js
@@ -1,4 +1,4 @@
-const filteredRecipes = (recipes, query) => {
+/*const filteredRecipes = (recipes, query) => {
 
 	const results = [];
     
@@ -43,4 +43,4 @@ const filteredRecipes = (recipes, query) => {
 			return results;
 
 		}
-};
+};*/
